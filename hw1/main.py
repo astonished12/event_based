00d68e895d79bb42927992da626eb146a447d028
xSkyripper <alexandru.cristea604@gmail.com>
2019-03-15 23:07:35 +0200

    Add subscribe input validation

--- hw1/main.py
+++ hw1/main.py
@@ -1,3 +1,4 @@
+import re
 import click
 import names
 import random
@@ -24,7 +25,6 @@ FIELDS = {
     }
 }
 
-
 @click.group()
 def cli():
     pass
@@ -42,8 +42,8 @@ def get_random_pub(fields_config):
 
     min_height = fields_config['height']['min']
     max_height = fields_config['height']['max']
-    max_delta_height = max_height - min_height
-    random_delta_height = random.random()
+    max_delta_ height = max_height - min_height
+    random_delta_height = random.random() * max_delta_height
     pub['height'] = round(float(min_height) + random_delta_height, 2)
 
     pub['eye-color'] = random.choice(fields_config['eye-color']['choices'])
@@ -62,6 +62,7 @@ def get_random_pub(fields_config):
 def publish(count):
     pubs = [get_random_pub(FIELDS) for _ in range(count)]
 
+    print('patient-name,DoB,height,eye-color,heart-rate')
     for pub in pubs:
         print('{},{},{},{},{}'
               .format(pub['patient-name'], pub['DoB'].strftime('%d-%m-%Y'), 
@@ -69,9 +70,30 @@ def publish(count):
 
 
 @cli.command('subscribe')
-def subscribe():
-    print('subscribe')
+@click.option('-n', '--count', required=True, type=click.INT)
+@click.option('-f', '--field', required=True, multiple=True)
+@click.option('-e', '--eq-field', required=True)
+def subscribe(count, field, eq_field):
+    fields_percentages = dict()
+    eq_field_percentage = None
 
+    escaped_fields_regex = '|'.join([re.escape(f) for f in FIELDS.keys()])
+    field_regex = r'^({})=([1-9]?[0-9]0?)%$'.format(escaped_fields_regex)
+    for f in field:
+        assert re.match(field_regex, f), \
+               'Wrong field {} format! Format: field-name=<0-100>%'.format(f)
+        field_name, percent = f.split('=')
+        percent = int(percent.replace('%', ''))
+        fields_percentages[field_name] = percent
+
+    assert re.match(field_regex, eq_field), \
+           'Wrong equality field {} format! Format: field-name=<0-100>%'.format(eq_field)
+    field_name, percent = eq_field.split('=')
+    percent = int(percent.replace('%', ''))
+    eq_field_percentage = (field_name, percent)
+
+    print(fields_percentages)
+    print(eq_field_percentage)
 
 if __name__ == "__main__":
     cli()
